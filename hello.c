266c33684022192eb605877abf5ed621f1146cdf
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2017-11-12 20:29:41 +0000

    runc list call

--- hello.c
+++ hello.c
@@ -10,23 +10,23 @@ asmlinkage long sys_hello(int container_action, char *action_arg) {
 
     if (container_action == 1) {
         printk("Running container create.\n");
-        argv[2] = "/bin/runc create test > /tmp/ca644_util_log";
+        argv[2] = "/bin/runc create test 2> /tmp/ca644_util_log";
     }
     else if (container_action == 2) {
         printk("Running container list.\n");
-        argv[2] = "/bin/runc list > /tmp/ca644_util_log";
+        argv[2] = "/bin/runc list 2>&1 > /tmp/ca644_util_log";
     }
     else if (container_action == 3) {
         printk("Running container start.\n");
-        argv[2] = "/bin/runc start > /tmp/ca644_util_log";
+        argv[2] = "/bin/runc start 2> /tmp/ca644_util_log";
     }
     else if (container_action == 4) {
         printk("Running container delete.\n");
-        argv[2] = "/bin/runc delete test > /tmp/ca644_util_log";
+        argv[2] = "/bin/runc delete test 2> /tmp/ca644_util_log";
     }
     else if (container_action == 6) {
         printk("Running container ps.\n");
-        argv[2] = "/bin/runc ps test > /tmp/ca644_util_log";
+        argv[2] = "/bin/runc ps test 2>&1 > /tmp/ca644_util_log";
     }
     else {
         return 666;
