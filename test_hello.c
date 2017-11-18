f97837e21478a1b8782ff78ddcbe4a02ef1f4982
Daniel Rejniak <daniel.rejniak@gmail.com>
2017-11-18 02:05:26 -0800

    Updated front-end to pass container

--- test_hello.c
+++ test_hello.c
@@ -16,68 +16,42 @@ static void display_file(const char *file_name) {
     }
 }
 
-int manage_container(int i) {
-    int manage_choice;
-    char container_id[12];
-    sprintf(container_id, "%d", i);
-    char *container_action_1 = "1";
-    char *container_action_2 = "2";
-    char *container_action_3 = "3";
+int manage_option(int i) {
+	
+        int container_id_size = 20;
+    	char container_id[container_id_size];
+    	long int ret_code;
+	int manage_choice;	
 
-    printf("\n --- Manage Container %d --- \n\n",i);
-    printf("1. Kill Container\n");
-    printf("2. Stop Container\n");
-    printf("3. Start Container\n\n");
-    printf("Manage Choice : ");
-    scanf("%d", &manage_choice);
+	if(i == 2) {
+		printf("Start Container With Id : ");
+    		scanf("%s", container_id);
+		printf("\nStarting Container...\n\n");
+                ret_code = syscall(333, i, container_id);
+      
+	}else if(i == 3) {
+		printf("Delete Container With Id : ");
+    		scanf("%d", &manage_choice);
+		printf("\nDeleting Container...\n\n");
+		ret_code = syscall(333, i, container_id);
 
-    if(manage_choice == 1) {
-        printf("\nKilling Container With Id: %d\n\n",i);
-        long int ret_code = syscall(333, container_action_1, container_id);
-        return ret_code;
+	}else if(i == 4) {
+		printf("List services inside container With Id : ");
+    		scanf("%s", container_id);
+		ret_code = syscall(333, i, container_id);
+
+
+	}else if(i == 5) {
+		printf("Create Container With Id : ");
+    		scanf("%s", container_id);
+                chdir("ca644_alpine");
+		char *arg[] = {"runc", "create", container_id, NULL};
+                printf("\nCreating Container...\n\n");
+                return execvp(arg[0], arg);	
+	}else {
+        	printf("\nInvalid Choice Exiting...\n\n");
+        	return 0;
     }
-    else if(manage_choice == 2){
-        printf("\nStoping Container With Id: %d\n\n",i);
-        long int ret_code = syscall(333, container_action_2, container_id);
-        return ret_code;
-    }
-    else if(manage_choice == 3){
-        printf("\nStarting Container With Id: %d\n\n",i);
-        long int ret_code = syscall(333, container_action_3, container_id);
-        return ret_code;
-    }
-    else {
-        printf("\nInvalid Choice Exiting...\n\n");
-        return 0;
-    }
-}
-
-int create_container() {
-
-    int container_type;
-
-    printf("1. Alpine Container \n");
-    printf("2. BusyBox Container \n");
-    printf("\nChoose Option From Menu Above 1-2: ");
-    scanf("%d", &container_type);
-
-
-    if(container_type == 1) {
-        printf("\nCreating Alpine container...\n\n");
-        chdir("ca644_alpine");
-    }
-    else if(container_type == 2) {
-        printf("\nNot implemented, but could be extended.\n\n");
-        return 1;
-    }
-    else {
-        printf("\nUnvalid container option\n\n");
-        return 1;
-    }
-
-
-    char *arg[] = {"runc", "create", "test", NULL};
-    return execvp(arg[0], arg);
 }
 
 int main() {
@@ -95,7 +69,7 @@ int main() {
     printf("3. Delete Container\n");
     printf("4. List services inside Container\n");
     printf("5. Create Container\n");
-    printf("Choose Option From Menu Above 1-3: ");
+    printf("Choose Option From Menu Above 1-5: ");
     scanf("%d", &menu_option);
 
     switch(menu_option) {
@@ -106,25 +80,20 @@ int main() {
             break;
         case(2):
             printf("\n --- Start Container --- \n\n");
-            if(fgets(container_id, container_id_size, stdin) != NULL) {
-                ret_code = syscall(333, menu_option, container_id);
-            }
+	    manage_option(menu_option);
             break;
         case(3):
             printf("\n --- Delete Container --- \n\n");
-            if(fgets(container_id, container_id_size, stdin) != NULL) {
-                ret_code = syscall(333, menu_option, container_id);
-            }
+	    manage_option(menu_option);
+
             break;
         case(4):
             printf("\n --- List services inside container --- \n\n");
-            if(fgets(container_id, container_id_size, stdin) != NULL) {
-                ret_code = syscall(333, menu_option, container_id);
-            }
+            manage_option(menu_option);
             break;
         case(5):
             printf("\n --- Create Container --- \n\n");
-            ret_code = create_container();
+            manage_option(menu_option);
             break;
         default:
             printf("\n Invalid Choice! \n");
