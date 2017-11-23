7789512f2e9f97abc63334687d5490f1bb151481
Ruben Vasconcelos <ruben.vasconcelos2@mail.dcu.ie>
2017-11-23 16:20:39 +0000

    More code refactoring.

--- test_hello.c
+++ test_hello.c
@@ -24,16 +24,8 @@ void getContainerID(char* container_id) {
 int manage_option(int option) {
     int container_id_size = 20;
     char container_id[container_id_size];
-    
-    if (option == 1) {
-        printf("\n --- List Containers --- \n\n");
-        int ret_code = syscall(333, option, NULL);
-        
-        if(ret_code == 0) {
-            display_file("/tmp/ca644_util_log");
-        }
-        return ret_code;
-    }
+
+    if (option == 1) { printf("\n --- List Containers --- \n\n"); }
     else if(option == 2) {
         printf("\n --- Create Container --- \n\n");
         getContainerID(container_id);
@@ -41,52 +33,23 @@ int manage_option(int option) {
         char *arg[] = {"runc", "create", container_id, NULL};
         return execvp(arg[0], arg);
     }
-    else if(option == 3) {
-        printf("\n --- Get Container State --- \n\n");
-        getContainerID(container_id);
-        int ret_code = syscall(333, option, container_id);
+    else if(option == 3) { printf("\n --- Get Container State --- \n\n"); }
+    else if(option == 4) { printf("\n --- Start Container --- \n\n"); }
+    else if(option == 5) { printf("\n --- List services inside container --- \n\n"); }
+    else if(option == 6) { printf("\n --- Pause Container Execution --- \n\n"); }
+    else if(option == 7) { printf("\n --- Resume Container Execution --- \n\n"); }
+    else if(option == 8) { printf("\n --- Kill Container --- \n\n"); }
+    else if(option == 9) { printf("\n --- Delete Stopped Container --- \n\n"); }
 
-        if(ret_code == 0) {
-            display_file("/tmp/ca644_util_log");
-        }
-        return ret_code;
-    }
-    else if(option == 4) {
-        printf("\n --- Start Container --- \n\n");
+    if(option != 1)
         getContainerID(container_id);
-        return syscall(333, option, container_id);
-    }
-    else if(option == 5) {
-        printf("\n --- List services inside container --- \n\n");
-        getContainerID(container_id);
-        int ret_code = syscall(333, option, container_id);
 
-        if(ret_code == 0) {
-            display_file("/tmp/ca644_util_log");
-        }
+    int ret_code = syscall(333, option, container_id);
 
-        return ret_code;
-    }
-    else if(option == 6) {
-        printf("\n --- Pause Container Execution --- \n\n");
-        getContainerID(container_id);
-        return syscall(333, option, container_id);
-    }
-    else if(option == 7) {
-        printf("\n --- Resume Container Execution --- \n\n");
-        getContainerID(container_id);
-        return syscall(333, option, container_id);
-    }
-    else if(option == 8) {
-        printf("\n --- Kill Container --- \n\n");
-        getContainerID(container_id);
-        return syscall(333, option, container_id);
-    }
-    else if(option == 9) {
-        printf("\n --- Delete Stopped Container --- \n\n");
-        getContainerID(container_id);
-        return syscall(333, option, container_id);
-    }
+    if(ret_code == 0 && (option == 1 || option == 3 || option == 5))
+        display_file("/tmp/ca644_util_log");
+
+    return ret_code;
 }
 
 int startMenu() {
@@ -116,7 +79,7 @@ int main() {
     int container_id_size = 20;
     int menu_option, ret_code;
     char container_id[container_id_size];
-    
+
     menu_option = startMenu();
 
     if (menu_option >= 1 && menu_option <= 9) {
